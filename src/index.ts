04e2681b5f20f8b0ce29d1e9418cbd242407c66f
José Siapo <jsiapo@unitru.edu.pe>
2020-02-27 01:51:38 -0500

    Create structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(3001);
+
+console.log("Server on port", 3001);
